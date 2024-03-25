e3e2c0249a5ba39f9cf0481e484e662423e03983
babut1 <jakub-kosc@o2.pl>
2024-03-26 00:30:07 +0100

    develop

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { SiteRouterProvider } from "./components/SiteRouterProvider";
+import ReactGA from "react-ga";
+ReactGA.initialize("G-FS43YKJPDG");
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <SiteRouterProvider />
